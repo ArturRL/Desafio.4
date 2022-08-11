9f16cbd8390874cbaa64b6705d6ca430b4ec0105
ArturRL <artur.rod.lopes@gmail.com>
2022-08-11 20:02:56 -0300

    Desafio Entregue

---
+++ script.js
@@ -0,0 +1,25 @@
+function danda(){
+    console.log("oi")
+}
+danda()
+
+////
+
+function tutu(nome){
+    console.log(`olá ${nome}`)
+}
+tutu("ARTUR")
+
+////
+
+function pao(nome,idade,estilodemusica){
+    console.log(`O meu nome é ${nome}, minha idade é ${idade}, o meu estilo musical é ${estilodemusica}`)
+}
+pao("Artur","18", "rock pesadão")
+
+////
+
+function mortadela (filme,musica){
+    console.log(`Um filme ${filme}, uma musica ${musica}`)
+}
+mortadela("Harry Potter", "Índios")
